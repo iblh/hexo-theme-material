948343aa8831443de7bd1de38daed1aa6125d74c
iocast <iocast@me.com>
2013-10-07 20:34:46 +0200

    master

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,19 +1,19 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
+  echo -e "Starting to update master\n"
 
   cp -R _site $HOME/_site
 
   cd $HOME
   git config --global user.email "iocast@me.com"
   git config --global user.name "iocast"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/iocast/iocast.github.io.git gh-pages > /dev/null
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/iocast/iocast.github.io.git master > /dev/null
 
   cd gh-pages
   cp -Rf $HOME/_site/* .
 
   git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
+  git push -fq origin master > /dev/null
 
   echo -e "Done magic with coverage\n"
 fi
