ef0c3d67ce6bab15416092a2da5939e05115d60c
cubesky <max_301@live.com>
2017-01-21 22:13:08 +0800

    refactor(load): code style

--- source/js/queue.js
+++ source/js/queue.js
@@ -8,23 +8,29 @@ function Queue() {
 
 
     function offer(element) {
-        if(this.debug) console.log("Offered a Queued Function.");
-        if(typeof element === "function") {
+        if(this.debug){
+            console.log('Offered a Queued Function.');
+        }
+        if(typeof element === 'function') {
             this.dataStore.push(element);
         } else {
-            console.log("You must offer a function.");
+            console.log('You must offer a function.');
         }
     }
 
     function poll() {
-        if(this.debug) console.log("Polled a Queued Function.");
+        if(this.debug){
+            console.log('Polled a Queued Function.');
+        }
         return this.dataStore.shift();
     }
 
     function execNext() {
-        var nextfunc=this.poll();
-        if(nextfunc != undefined) {
-            if(this.debug) console.log("Run a Queued Function.");
+        var nextfunc = this.poll();
+        if(nextfunc !== undefined) {
+            if(this.debug){
+                console.log('Run a Queued Function.');
+            }
             nextfunc();
         }
     }
