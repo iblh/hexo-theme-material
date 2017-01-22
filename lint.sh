c322f890afb15891048889be7df6afe7d5250dd3
pidupuis <pidupuis@protonmail.com>
2017-01-22 17:57:22 +0100

    chore(lint): add js linter

--- lint.sh
+++ lint.sh
@@ -11,3 +11,9 @@ for f in $(find ./source/ -name "*.css" | grep -v ".min.css"); do
   echo $f;
   node_modules/.bin/sass-lint -v -q $f;
 done
+
+# Lint JS files
+for f in $(find ./source/ -name "*.js" | grep -v ".min.js"); do
+  echo $f;
+  node_modules/.bin/eslint $f;
+done
