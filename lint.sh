b90c0220b92712792fba58f2f18f13f3ca28dbe6
pidupuis <pidupuis@protonmail.com>
2017-01-22 17:43:32 +0100

    chore(lint): add css linter

--- lint.sh
+++ lint.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 
-for f in $(find ./layout -name '*.ejs'); do
+# Lint EJS files
+for f in $(find ./layout -name "*.ejs"); do
   echo $f;
   node_modules/.bin/ejslint $f;
+done
+
+# Lint CSS files
+for f in $(find ./source/ -name "*.css" | grep -v ".min.css"); do
+  echo $f;
+  node_modules/.bin/sass-lint -v -q $f;
 done
