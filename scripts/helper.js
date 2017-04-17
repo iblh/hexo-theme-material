34589ac1409ace525be65b1dd436edd1daafa3a5
eyhn <cneyhn@gmail.com>
2017-04-17 09:46:04 +0800

    feat(cache): localstorage cache js

---
+++ scripts/helper.js
@@ -0,0 +1,7 @@
+hexo.extend.helper.register('jsHex', function(){
+  return require("./lib/js_hex.js").call(hexo,...arguments);
+});
+
+hexo.extend.helper.register('jsLsload', function(){
+  return require("./lib/js_lsload.js").call(hexo,...arguments);
+});
