864f99a587ace330c35b4029387a6d2020851be3
Ghou <ho0thy0oy@gmail.com>
2017-04-26 23:05:02 +0800

    style(callout): add callout tag
    A custom tag for the Bootstrap callout style is available for material theme.

---
+++ scripts/tag_callout.js
@@ -0,0 +1,24 @@
+'use strict';
+
+hexo.extend.tag.register('note', function(args, content) {
+  var calloutType = '';
+  var header = '';
+  var result = '';
+  if (args.length) {
+    if (args[0]=='default'||args[0]=='primary'||args[0]=='success'||args[0]=='danger'||args[0]=='warning'||args[0]=='info'){
+      calloutType += args.shift();
+      header += '<div class="bs-callout-title">' + args.join(' ') + '</div>';
+    }
+    else {
+      calloutType += 'default';
+      header += '<div class="bs-callout-title">' + args.join(' ') + '</div>';
+    }
+  }
+  else {
+    calloutType += 'default';
+  }
+  result += '<div class="bs-callout bs-callout-' + calloutType + '">' + header;
+  result += hexo.render.renderSync({text: content, engine: 'markdown'});
+  result += '</div>';
+  return result;
+}, true);
