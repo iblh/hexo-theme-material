7ec7bc05c206a8c9f936dde1342ea2dd223bb153
Ghou <ho0thy0oy@gmail.com>
2017-05-01 14:23:51 +0800

    style(callout): refactor callout style
    Try to materialize callout style.
    Resolve the merge conflicts.

--- scripts/tag_callout.js
+++ scripts/tag_callout.js
@@ -6,18 +6,23 @@ hexo.extend.tag.register('note', function(args, content) {
   var result = '';
   if (args.length) {
     if (args[0]=='default'||args[0]=='primary'||args[0]=='success'||args[0]=='danger'||args[0]=='warning'||args[0]=='info'){
-      calloutType += args.shift();
-      header += '<div class="bs-callout-title">' + args.join(' ') + '</div>';
+      if (args[1]) {
+        calloutType += args.shift();
+        header += '<div class="md-callout-title">' + args.join(' ') + '</div>';
+      }
+      else {
+        calloutType += args[0];
+      }
     }
     else {
       calloutType += 'default';
-      header += '<div class="bs-callout-title">' + args.join(' ') + '</div>';
+      header += '<div class="md-callout-title">' + args.join(' ') + '</div>';
     }
   }
   else {
     calloutType += 'default';
   }
-  result += '<div class="bs-callout bs-callout-' + calloutType + '">' + header;
+  result += '<div class="md-callout md-callout-' + calloutType + '">' + header;
   result += hexo.render.renderSync({text: content, engine: 'markdown'});
   result += '</div>';
   return result;
