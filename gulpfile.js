1d82c239959549adf7a4d0bc533f501af1d2db95
neoFelhz <neofelhz@gmail.com>
2017-09-19 12:17:48 +0800

    chore(gulp): minor changes - remove cb - fix dictories

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@ let cleanCSS = require('gulp-clean-css');
 let uglify = require('gulp-uglify');
 let rename = require("gulp-rename");
 
-gulp.task('minify-js', function (cb) {
-  return gulp.src('dist/js/*.js')
-	.pipe(rename({suffix: '.min'}))
+gulp.task('minify-js', () => {
+  return gulp.src('dist/js/**/*.js')
+    .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest('source/js'));
 });
 
 gulp.task('minify-css', () => {
-  return gulp.src('dist/css/*.css')
+  return gulp.src('dist/css/**/*.css')
 	.pipe(rename({suffix: '.min'}))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(cleanCSS())
     .pipe(gulp.dest('source/css'));
 });
 
-gulp.task('default', function() {
+gulp.task('default', () => {
     gulp.start('minify-js', 'minify-css'); 
 });
